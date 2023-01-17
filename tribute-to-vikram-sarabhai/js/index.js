0592ede7782211953bc915f0c84032ac3e498be5
PriyanshuMaurya <mauryapriyanshu-cmpn@atharvacoe.ac.in>
2023-01-17 22:34:04 +0530

    added card for facts as needed and added final touch to entire page

--- tribute-to-vikram-sarabhai/js/index.js
+++ tribute-to-vikram-sarabhai/js/index.js
@@ -1,8 +1,3 @@
-// document
-//   .querySelector("#darkmode")
-//   .addEventListener("change", function (event) {
-//     document.querySelector("body").classList.toggle("dark");
-//   });
 
 const facts = [
   "He belonged to an affluent family in Gujarat who owned many mills and industries in India. His family  was known for the social work that they did for the underprivileged people of the society.",
@@ -17,19 +12,114 @@ const facts = [
   "For his visionary work, he was awarded Padma Bhushan in 1966 and Padma Vibhushan in 1972 (after his demise).",
 ];
 
-var cardContainer = document.querySelector("#card-container");
+var cardContainer = document.querySelector(".slide_group");
 
 for (let i = 0; i < facts.length; i++) {
   var card = `
-    <div class="card">
-    <h2>${i + 1}</h2>
-    <hr />
-    <p>
-    ${facts[i]}
-    </p>
-  </div>
-    `;
+      <div class="slide basic-card basic-card-aqua">
+          <div class="card-content">
+              <span class="card-title">${i + 1}</span>
+              <hr>
+              <p class="card-text">
+                  ${facts[i]}
+              </p>
+          </div>
+      </div>
+      `;
   cardContainer.innerHTML += card;
   // cardContainer.appendChild(card);
   // console.trace(i);
 }
+
+$(".slider").each(function () {
+  var $this = $(this);
+  var $group = $this.find(".slide_group");
+  var $slides = $this.find(".slide");
+  var bulletArray = [];
+  var currentIndex = 0;
+  var timeout;
+
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+
+    advance();
+
+    if ($group.is(":animated") || currentIndex === newIndex) {
+      return;
+    }
+
+    bulletArray[currentIndex].removeClass("active");
+    bulletArray[newIndex].addClass("active");
+
+    if (newIndex > currentIndex) {
+      slideLeft = "100%";
+      animateLeft = "-100%";
+    } else {
+      slideLeft = "-100%";
+      animateLeft = "100%";
+    }
+
+    $slides.eq(newIndex).css({
+      display: "block",
+      left: slideLeft,
+    });
+    $group.animate(
+      {
+        left: animateLeft,
+      },
+      function () {
+        $slides.eq(currentIndex).css({
+          display: "none",
+        });
+        $slides.eq(newIndex).css({
+          left: 0,
+        });
+        $group.css({
+          left: 0,
+        });
+        currentIndex = newIndex;
+      }
+    );
+  }
+
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      if (currentIndex < $slides.length - 1) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 4000);
+  }
+
+  $(".next_btn").on("click", function () {
+    if (currentIndex < $slides.length - 1) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+
+  $(".previous_btn").on("click", function () {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
+  });
+
+  $.each($slides, function (index) {
+    var $button = $('<a class="slide_btn">&bull;</a>');
+
+    if (index === currentIndex) {
+      $button.addClass("active");
+    }
+    $button.on("click", function () {
+      move(index);
+    });
+    bulletArray.push($button);
+  });
+
+  advance();
+});
